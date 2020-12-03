d24a85e45d1d55a5a7b54ce0289ed34612be9aea
William Schutte <williamdschutte@gmail.com>
2020-12-03 09:16:58 -0500

    Reset for pull request

--- middleware/rateLimiter.js
+++
@@ -1,8 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-const limiter = rateLimit({
-  windowMs: 2 * 60 * 1000,
-  max: 50,
-});
-
-module.exports = limiter;
--- routes/users.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { getUserInfo } = require('../controllers/users');
-
-router.get('/me', getUserInfo);
-
-module.exports = router;
